9732756e8354cad7828dd68face97cab2497abdc
Mulodji <georgeclemente@gmail.com>
2024-12-19 17:13:33 +0100

    Update script.js - another approach with explicit back btn

--- script.js
+++ script.js
@@ -8,54 +8,47 @@ document.addEventListener('DOMContentLoaded', function() {
     let currentPlayingVideo = null;
 
 
-    function showMosaic() {
-        mosaicContainer.classList.remove('collapsed');
-        videoCard.classList.remove('collapsed');
-         backButton.style.display = 'none';
-    }
-
-    function hideMosaic() {
-       mosaicContainer.classList.add('collapsed');
-       videoCard.classList.add('collapsed');
-        backButton.style.display = 'block';
-    }
-
     thumbContainers.forEach(thumb => {
         thumb.addEventListener('click', function() {
             const videoURL = this.getAttribute('data-video');
-            
-           if (currentPlayingVideo === videoURL) {
+              if (currentPlayingVideo === videoURL) {
                // Already playing this video, no need to do anything
                 return;
             }
-            
-            if(currentPlayingVideo) {
+             if(currentPlayingVideo) {
                // Stop the currently playing video
                videoPlayer.src = '';
                playerContainer.style.display = 'none';
            }
-
+           
+           mosaicContainer.style.display = 'none';
+            videoCard.classList.add('collapsed');
+           backButton.style.display = 'block';
             videoPlayer.src = videoURL;
             playerContainer.style.display = 'block';
             videoPlayer.play();
-            hideMosaic();  // <------- HERE: This will hide the mosaic container.
-            currentPlayingVideo = videoURL;
+           currentPlayingVideo = videoURL;
         });
     });
 
-      videoPlayer.addEventListener('ended', function() {
-          showMosaic();
-        currentPlayingVideo = null;
-      });
+     videoPlayer.addEventListener('ended', function() {
+        mosaicContainer.style.display = 'grid';
+          videoCard.classList.remove('collapsed');
+          backButton.style.display = 'none';
+         currentPlayingVideo = null;
 
-     backButton.addEventListener('click', function() {
-          videoPlayer.src = '';
-           playerContainer.style.display = 'none';
-          showMosaic();
-           currentPlayingVideo = null;
-      });
+    });
+
+    backButton.addEventListener('click', function() {
+        videoPlayer.src = '';
+        playerContainer.style.display = 'none';
+           mosaicContainer.style.display = 'grid';
+         videoCard.classList.remove('collapsed');
+        backButton.style.display = 'none';
+        currentPlayingVideo = null;
+    });
 
     videoPlayer.addEventListener('click', function(event) {
-           event.stopPropagation();
-      });
+        event.stopPropagation();
+    });
 });
