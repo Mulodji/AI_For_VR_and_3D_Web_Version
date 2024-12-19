abea189d40fa6aa4d6e308a1af7ca3812a4c883b
Mulodji <georgeclemente@gmail.com>
2024-12-19 16:49:50 +0100

    Update script.js - implementing the back button, and the mosaic toggling

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const playerContainer = document.getElementById('playerContainer');
     const videoPlayer = document.getElementById('videoPlayer');
     const thumbContainers = document.querySelectorAll('.thumb-container');
-     const mosaicContainer = document.getElementById('mosaicContainer');
-     const backButton = document.getElementById('backButton');
+    const mosaicContainer = document.getElementById('mosaicContainer');
+    const backButton = document.getElementById('backButton');
     let currentPlayingVideo = null;
 
 
@@ -17,13 +17,14 @@ document.addEventListener('DOMContentLoaded', function() {
     function hideMosaic() {
        mosaicContainer.classList.add('collapsed');
        videoCard.classList.add('collapsed');
-       backButton.style.display = 'block';
+        backButton.style.display = 'block';
     }
 
     thumbContainers.forEach(thumb => {
         thumb.addEventListener('click', function() {
             const videoURL = this.getAttribute('data-video');
-             if (currentPlayingVideo === videoURL) {
+            
+           if (currentPlayingVideo === videoURL) {
                // Already playing this video, no need to do anything
                 return;
             }
@@ -34,10 +35,10 @@ document.addEventListener('DOMContentLoaded', function() {
                playerContainer.style.display = 'none';
            }
 
-             videoPlayer.src = videoURL;
-             playerContainer.style.display = 'block';
+            videoPlayer.src = videoURL;
+            playerContainer.style.display = 'block';
             videoPlayer.play();
-            hideMosaic();
+            hideMosaic();  // <------- HERE: This will hide the mosaic container.
             currentPlayingVideo = videoURL;
         });
     });
@@ -51,7 +52,7 @@ document.addEventListener('DOMContentLoaded', function() {
           videoPlayer.src = '';
            playerContainer.style.display = 'none';
           showMosaic();
-          currentPlayingVideo = null;
+           currentPlayingVideo = null;
       });
 
     videoPlayer.addEventListener('click', function(event) {
