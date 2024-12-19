c9d24fba4a8c287ae7cd6f174fe5930b9d337b12
Mulodji <Mulodji@users.noreply.github.com>
2024-12-19 20:48:58 +0000

    deploy: d8c5d983caa46cab6d6f3266a2b1d9fc7d2134f2

--- script.js
+++ script.js
@@ -4,70 +4,75 @@ document.addEventListener('DOMContentLoaded', function() {
     const videoPlayer = document.getElementById('videoPlayer');
     const thumbContainers = document.querySelectorAll('.thumb-container');
     const mosaicContainer = document.getElementById('mosaicContainer');
-     const backButton = document.getElementById('backButton');
-     const bottomButton = document.getElementById('bottomButton');
-     const slidesContainer = document.getElementById('slidesContainer');
-      const slidesPlayer = document.getElementById('slidesPlayer');
-     const slidesBackButton = document.getElementById('slidesBackButton');
+    const backButton = document.getElementById('backButton');
+    const bottomButton = document.getElementById('bottomButton');
+    const topButton = document.getElementById('topButton');
+    const slidesContainer = document.getElementById('slidesContainer');
+    const slidesPlayer = document.getElementById('slidesPlayer');
+    const slidesBackButton = document.getElementById('slidesBackButton');
     let currentPlayingVideo = null;
 
 
     thumbContainers.forEach(thumb => {
         thumb.addEventListener('click', function() {
             const videoURL = this.getAttribute('data-video');
-              if (currentPlayingVideo === videoURL) {
-               // Already playing this video, no need to do anything
+            if (currentPlayingVideo === videoURL) {
+                // Already playing this video, no need to do anything
                 return;
             }
              if(currentPlayingVideo) {
-               // Stop the currently playing video
-               videoPlayer.src = '';
-               playerContainer.style.display = 'none';
-           }
-
-           mosaicContainer.style.display = 'none';
-            videoCard.classList.add('collapsed');
-           backButton.style.display = 'block';
+                // Stop the currently playing video
+                videoPlayer.src = '';
+                playerContainer.style.display = 'none';
+            }
+            mosaicContainer.style.display = 'none';
+             videoCard.classList.add('collapsed');
+            backButton.style.display = 'block';
             videoPlayer.src = videoURL;
             playerContainer.style.display = 'block';
             videoPlayer.play();
-           currentPlayingVideo = videoURL;
+             currentPlayingVideo = videoURL;
         });
     });
 
-     videoPlayer.addEventListener('ended', function() {
-        mosaicContainer.style.display = 'grid';
-          videoCard.classList.remove('collapsed');
-          backButton.style.display = 'none';
+    videoPlayer.addEventListener('ended', function() {
+       mosaicContainer.style.display = 'grid';
+        videoCard.classList.remove('collapsed');
+        backButton.style.display = 'none';
         currentPlayingVideo = null;
-
     });
 
-    backButton.addEventListener('click', function() {
-        videoPlayer.src = '';
-         playerContainer.style.display = 'none';
-           mosaicContainer.style.display = 'grid';
-         videoCard.classList.remove('collapsed');
-         backButton.style.display = 'none';
-         currentPlayingVideo = null;
+     backButton.addEventListener('click', function() {
+         videoPlayer.src = '';
+        playerContainer.style.display = 'none';
+        mosaicContainer.style.display = 'grid';
+          videoCard.classList.remove('collapsed');
+        backButton.style.display = 'none';
+        currentPlayingVideo = null;
     });
 
     videoPlayer.addEventListener('click', function(event) {
         event.stopPropagation();
     });
 
-   bottomButton.addEventListener('click', function(){
-      mosaicContainer.style.display = 'none';
-        videoCard.classList.add('collapsed');
+
+    bottomButton.addEventListener('click', function(){
+       mosaicContainer.style.display = 'none';
+       videoCard.classList.add('collapsed');
         slidesContainer.style.display = 'block';
-         slidesBackButton.style.display = 'block';
-   })
+        slidesBackButton.style.display = 'block';
+         topButton.style.display = 'none'; // Hide the top button
+       bottomButton.style.display = 'none'; // Hide the bottom button
 
-    slidesBackButton.addEventListener('click', function() {
-           slidesContainer.style.display = 'none';
-            mosaicContainer.style.display = 'grid';
-          videoCard.classList.remove('collapsed');
-          slidesBackButton.style.display = 'none';
+   });
+
+   slidesBackButton.addEventListener('click', function() {
+        slidesContainer.style.display = 'none';
+        mosaicContainer.style.display = 'grid';
+        videoCard.classList.remove('collapsed');
+        slidesBackButton.style.display = 'none';
+          topButton.style.display = 'flex';  // Show the top button again
+        bottomButton.style.display = 'block';  // Show the bottom button again
+
     });
-
 });
